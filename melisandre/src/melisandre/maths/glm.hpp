f907563f0d421ceadb13f90f177e91810f6583f5
Laurent Noel <laurent.noel@ubisoft.com>
2016-01-26 16:14:40 +0100

    Add numeric.hpp header and move functions from glm.hpp to it

--- melisandre/src/melisandre/maths/glm.hpp
+++ melisandre/src/melisandre/maths/glm.hpp
@@ -13,26 +13,4 @@
 
 namespace mls {
 
-using glm::radians;
-using glm::degrees;
-
-using glm::cos;
-using glm::sin;
-using glm::tan;
-using glm::abs;
-using glm::floor;
-using glm::sign;
-using glm::max;
-using glm::min;
-using glm::clamp;
-using glm::fract;
-using glm::pow;
-using glm::sqrt;
-
-using glm::isnan;
-using glm::isinf;
-using glm::isfinite;
-using glm::isPowerOfTwo;
-
-
 }
---
+++ melisandre/src/melisandre/maths/numeric.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "glm.hpp"
+
+namespace mls {
+
+using glm::radians;
+using glm::degrees;
+
+using glm::cos;
+using glm::sin;
+using glm::tan;
+using glm::abs;
+using glm::floor;
+using glm::sign;
+using glm::max;
+using glm::min;
+using glm::clamp;
+using glm::fract;
+using glm::pow;
+using glm::sqrt;
+
+using glm::isnan;
+using glm::isinf;
+using glm::isfinite;
+using glm::isPowerOfTwo;
+
+}
